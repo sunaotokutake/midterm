72640a88e97a693c38586073f59c9a06450d84a8
sunaotokutake <tokutakesunao@net84-dhcp-205.sfc.keio.ac.jp>
2016-06-09 12:17:57 +0900

    中間試験に回答した。
    中間試験に回答した。

---
+++ 04/04.js
@@ -0,0 +1,7 @@
+function xor(a,b){
+	    if((false,false) || (true,true)){
+		    return false;
+	}else if(!(false,false) || !(true,true)){
+		    return true;
+	}
+}
---
+++ 05/05.js
@@ -0,0 +1,7 @@
+function strlen(text_a){
+	return text_a.length;
+}
+
+function concat(text_a,text_b){
+	return text_a + text_b;
+}
